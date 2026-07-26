a587540e24592763240a0f3d98ed2adfb0e2ed2e
Anna Demyanovich <makarenko-an@part-kom.ru>
2026-07-26 16:53:19 +0300

    fix: Disable DevTools in production config

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+  devtools: { enabled: process.env.NODE_ENV !== 'production' },
   modules: ['@pinia/nuxt', '@nuxt/eslint'],
   eslint: {
     config: {
